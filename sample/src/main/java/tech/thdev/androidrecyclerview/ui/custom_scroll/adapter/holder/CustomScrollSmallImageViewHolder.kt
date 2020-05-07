304c63830a30162763425d77b80638fb864817a6
Taehwan <taehwan@thdev.net>
2020-05-08 01:34:50 +0900

    layout 정리중

--- sample/src/main/java/tech/thdev/androidrecyclerview/ui/custom_scroll/adapter/holder/CustomScrollSmallImageViewHolder.kt
+++ sample/src/main/java/tech/thdev/androidrecyclerview/ui/custom_scroll/adapter/holder/CustomScrollSmallImageViewHolder.kt
@@ -3,8 +3,8 @@ package tech.thdev.androidrecyclerview.ui.custom_scroll.adapter.holder
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_custom_scroll_image.view.*
 import kotlinx.android.synthetic.main.item_large_view.view.tv_message
-import kotlinx.android.synthetic.main.item_small_image.view.*
 import tech.thdev.androidrecyclerview.R
 import tech.thdev.androidrecyclerview.data.LocalImage
 import tech.thdev.support.widget.adapter.simple.holder.BaseViewHolder
@@ -15,7 +15,7 @@ import tech.thdev.support.widget.adapter.simple.holder.BaseViewHolder
 class CustomScrollSmallImageViewHolder(
     parent: ViewGroup,
     adapter: RecyclerView.Adapter<*>
-) : BaseViewHolder<LocalImage?>(R.layout.item_small_image, parent, adapter) {
+) : BaseViewHolder<LocalImage?>(R.layout.item_custom_scroll_image, parent, adapter) {
 
     override fun onBindViewHolder(item: LocalImage?, position: Int) {
         if (item == null) return
